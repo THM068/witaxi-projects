693bbe8dbf39d4793e6d5e7841974051b034fb4b
Thando Mafela <thando.mafela@sky.uk>
2025-01-09 14:30:51 +0000

    blnk-sdk

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name="witaxi-api"
+
+include("blnk-sdk")
+include("web:api")
---
+++ web/api/src/main/java/witaxi/Application.java
@@ -0,0 +1,9 @@
+package witaxi;
+import io.micronaut.runtime.Micronaut;
+public class Application {
+
+    public static void main(String[] args) {
+        blnk.witaxi.Library.someLibraryMethod();
+        Micronaut.run(Application.class, args);
+    }
+}
