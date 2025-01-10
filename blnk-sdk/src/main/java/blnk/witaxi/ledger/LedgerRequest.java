6251773215f6125a07960c77874c3745549da361
Thando Mafela <thando.mafela@sky.uk>
2025-01-10 00:47:04 +0000

    cleanup

--- blnk-sdk/src/main/java/blnk/witaxi/ledger/LedgerRequest.java
+++ blnk-sdk/src/main/java/blnk/witaxi/ledger/LedgerRequest.java
@@ -1,9 +1,6 @@
 package blnk.witaxi.ledger;
 
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
-
-import java.util.Map;
 @Serdeable
 public record LedgerRequest(String name, Ledger_Meta meta_data) {
 }
